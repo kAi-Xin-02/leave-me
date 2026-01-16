db30704763e385af91e69670b961206946d7d6b7
kAI Xin <star.light0x.2@gmail.conm>
2026-01-16 21:06:39 +0530

    Add config.js for live site - fix authentication

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://pbrbmjpsynpmshmysmzq.supabase.co',
+    SUPABASE_ANON_KEY: 'sb_publishable_zwl7FMC26oSvybJ5ALfd1Q_2rOM6mLe'
+};
+
+window.APP_CONFIG = CONFIG;
